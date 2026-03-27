03558fb85b7831d4629f537f56da4b1893e4de4a
CandySanjo <CandySanjo@outlook.com>
2026-03-27 15:32:49 +0800

    docs: update function docs.

--- server_scripts/add_loot.js
+++ server_scripts/add_loot.js
@@ -1,3 +1,10 @@
+/**
+ * 提供含有权重的指定物品标签中的所有物品
+ * @param  {String} tag 物品标签名
+ * @param  {Number} chance 物品出现的概率权重
+ * @return {*[]} 含有指定物品标签中所有物品的数组，每个物品都有一个概率权重
+ */
+
 function itemsOfTag(tag, chance) {
     let items = []
     Ingredient.of(tag).itemIds.forEach(id => {
