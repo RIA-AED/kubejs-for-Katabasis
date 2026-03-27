40e18d9cb7402afe91cd74ee4a47f9d923d9ef69
CandySanjo <CandySanjo@outlook.com>
2026-03-28 00:09:25 +0800

    feat: add support for item regex filtering in loot table

--- server_scripts/add_loot.js
+++ server_scripts/add_loot.js
@@ -2,22 +2,31 @@
  * 提供含有权重的指定物品标签中的所有物品
  * @param  {String} tag 物品标签名
  * @param  {Number} chance 物品出现的概率权重
- * @return {*[]} 含有指定物品标签中所有物品的数组，每个物品都有一个概率权重
+ * @return {Internal.Item[]} 含有指定物品标签中所有物品的数组，每个物品都有一个概率权重
  */
-
 function itemsOfTag(tag, chance) {
-    let items = []
-    Ingredient.of(tag).itemIds.forEach(id => {
-        items.push(Item.of(id).withChance(chance))
-    })
-    return items;
+    return Ingredient.of(tag).itemIds.map(id =>
+        Item.of(id).withChance(chance)
+    )
+}
+
+/**
+ * 提供含有权重的指定物品正则表达式中的所有物品
+ * @param  {RegExp} regExp 物品正则表达式
+ * @param  {Number} chance 物品出现的概率权重
+ * @return {Internal.Item[]} 含有指定物品正则表达式中所有物品的数组，每个物品都有一个概率权重
+ */
+function itemsOfRegex(regExp, chance) {
+    return Ingredient.all.itemIds
+        .filter(id => regExp.test(id))
+        .map(id => Item.of(id).withChance(chance))
 }
 
 LootJS.modifiers((event) => {
     event.addLootTableModifier('katabasis:chests/abc').pool((pool) => {
         pool.rolls([1,27])
         pool.randomChance(0.5).addWeightedLoot(
-            itemsOfTag('#minecraft:logs', 12.5)
+            itemsOfRegex(/:[a-zA-Z0-9]+_log$/, 12.5)
         ).limitCount([16,48])
     })
 })
