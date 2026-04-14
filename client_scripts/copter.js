8db30d8c7a7114d1b2bdff673f240e94ada5563e
ignis <15290520+ignisano@user.noreply.gitee.com>
2026-04-14 19:54:42 +0800

    fix(client_scripts): 修复直升机物品右键冷却判断逻辑
    - 修改右键事件处理，先判断手中物品是否为直升机
    - 仅在冷却生效且手持直升机时阻止动作
    - 修正了冷却检测条件，避免误触发逻辑错误

--- client_scripts/copter.js
+++ client_scripts/copter.js
@@ -1,4 +1,6 @@
 ItemEvents.rightClicked("kubejs:copter",event=>{
-    if(event.player.cooldowns.isOnCooldown("kubejs:copter"))return
+    if(event.player.getItemInHand(event.getHand())=="kubejs:copter" &&
+        event.player.cooldowns.isOnCooldown("kubejs:copter"))
+        return
     event.player.addMotion(0,1.5,0)
 })
