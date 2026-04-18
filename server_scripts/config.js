01514d781a4d86a390108b50f4b3c3f3c0e37ef5
liziluyu <2498281038@qq.com>
2026-04-19 01:04:59 +0800

    feat:掉落物清除转为定时

--- server_scripts/config.js
+++ server_scripts/config.js
@@ -13,9 +13,8 @@ let config = {
         "podFinalFallingHeight": 20
     },
     "ItemClean": {
-        "max_item_size": 500,
-        "item_clean_waitfortick": 60,
-        "item_clean_size": 10000
+        "item_clean_interval_tick": 500,
+        "item_clean_waitfortick": 60
     }
 }
 
--- server_scripts/item_clean.js
+++ server_scripts/item_clean.js
@@ -1,14 +1,9 @@
-
-
-let $itemClass = Java.loadClass("net.minecraft.world.entity.item.ItemEntity")
-LevelEvents.tick(event => {
-    let block = event.level.getBlock(0, 0, 0)
-    let items = event.level.getEntitiesOfClass($itemClass, AABB.ofBlock(block.pos).inflate(global.config.ItemClean.item_clean_size))
-    if (items.size() >= global.config.ItemClean.max_item_size) {
+ServerEvents.loaded(event => {
+    event.server.scheduleRepeatingInTicks(global.config.ItemClean.item_clean_interval_tick, function (callback) {
         event.server.tell("掉落物超过阈值！即将进行清理")
         event.server.scheduleInTicks(global.config.ItemClean.item_clean_waitfortick, function (callback) {
             event.server.runCommandSilent(`kill @e[type=item]`)
             event.server.tell("掉落物清理完成")
         })
-    }
+    })
 })
