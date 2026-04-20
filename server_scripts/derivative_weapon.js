6fa820b2600d620547752d565b72706371bb3006
Zhaoxi_Cor <1362740553@qq.com>
2026-04-20 20:56:20 +0800

    fix:衍生武器注册名修改

--- server_scripts/derivative_weapon.js
+++ server_scripts/derivative_weapon.js
@@ -1,32 +1,32 @@
 function getRandomInt() {
-    const minCeiled = Math.ceil(1);
-    const maxFloored = Math.floor(101);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+   const minCeiled = Math.ceil(1);
+   const maxFloored = Math.floor(101);
+   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
 }
 
 EntityEvents.hurt(event => {//圣光
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:aura_snake_strike') return
+    if (player.mainHandItem.id != 'pointblank:st_snake_strike_1') return
     event.entity.potionEffects.add('minecraft:instant_health', 1, 1)
 })
 EntityEvents.hurt(event => {//盛世
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:halcyon_k80') return
+    if (player.mainHandItem.id != 'pointblank:cr_k80_1') return
     event.entity.potionEffects.add('minecraft:absorption', 400, 1)
     event.entity.potionEffects.add('minecraft:regeneration', 200, 1)
 })
 EntityEvents.hurt(event => {//民团
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:posse_prism') return
+    if (player.mainHandItem.id != 'pointblank:e_prism_1') return
     event.entity.potionEffects.add('minecraft:resistance', 2400, 0)
 })
 EntityEvents.hurt(event => {//昏暗
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:murky_cross_star') return
+    if (player.mainHandItem.id != 'pointblank:x_cross_star_1') return
     event.entity.potionEffects.add('minecraft:resistance', 2400, 1)
     event.entity.potionEffects.add('minecraft:speed', 2400, 0)
     event.entity.potionEffects.add('minecraft:jump_boost', 2400, 0)
@@ -35,15 +35,15 @@ EntityEvents.hurt(event => {//昏暗
 EntityEvents.hurt(event => {//盗墓者
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:ghoul_sar') return
+    if (player.mainHandItem.id != 'pointblank:cr_sar_1') return
     player.heal(1)
 })
 
 EntityEvents.hurt(event => {//偏振
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:polarized_flea'
-        && player.mainHandItem.id != 'pointblank:polarized_medusa'
+    if (player.mainHandItem.id != 'pointblank:cr_flea_1'
+        && player.mainHandItem.id != 'pointblank:x_medusa_1'
     ) return
     player.potionEffects.add('spore:corrosion', 100, 6)
 })
@@ -51,25 +51,25 @@ EntityEvents.hurt(event => {//偏振
 EntityEvents.hurt(event => {//地蚀
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:umbra_stifle'
-        && player.mainHandItem.id != 'pointblank:umbra_jpot_ii9'
-        && player.mainHandItem.id != 'pointblank:umbra_vortex'
+    if (player.mainHandItem.id != 'pointblank:st_stifle_1'
+        && player.mainHandItem.id != 'pointblank:cr_jpot_ii9_1'
+        && player.mainHandItem.id != 'pointblank:e_vortex_1'
     ) return
-    if (getRandomInt() > 95 && event.entity.hasEffect('spore:corrosion')) {
+    if (getRandomInt() > 95 && event.entity.hasEffect('spore:corrosion')){
         event.entity.potionEffects.add('spore:corrosion', 200, 3)
     }
     if (event.entity.hasEffect('spore:corrosion')) return
-    if (getRandomInt() < 20) {
+    if (getRandomInt() < 20){
         event.entity.potionEffects.add('spore:corrosion', 200, 1)
-    }
+    }    
 })
 EntityEvents.hurt(event => {//炼狱
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != "pointblank:inferno_rasword"
-        && player.mainHandItem.id != "pointblank:inferno_shock"
-        && player.mainHandItem.id != "pointblank:inferno_spark"
-        && player.mainHandItem.id != "pointblank:inferno_protocol"
+    if (player.mainHandItem.id != "pointblank:st_rasword_1"
+        && player.mainHandItem.id != "pointblank:cr_shock_1"
+        && player.mainHandItem.id != "pointblank:e_spark_1"
+        && player.mainHandItem.id != "pointblank:x_protocol_1"
     ) return
     if (getRandomInt() >= 20) return
     event.entity.mergeNbt(`{Fire:200}`)
@@ -77,42 +77,42 @@ EntityEvents.hurt(event => {//炼狱
 EntityEvents.hurt(event => {//骤停
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:fleeting_sandstorm'
-        && player.mainHandItem.id != 'pointblank:fleeting_surge'
+    if (player.mainHandItem.id != 'pointblank:st_sandstorm_1'
+        && player.mainHandItem.id != 'pointblank:x_surge_1'
     ) return
-    if (getRandomInt() > 95 && event.entity.hasEffect('minecraft:slowness')) {
+    if (getRandomInt() > 95 && event.entity.hasEffect('minecraft:slowness')){
         event.entity.potionEffects.add('minecraft:slowness', 200, 2)
     }
     if (event.entity.hasEffect('minecraft:slowness')) return
-    if (getRandomInt() < 20) {
+    if (getRandomInt() < 20){
         event.entity.potionEffects.add('minecraft:slowness', 200, 0)
-    }
+    } 
 })
 EntityEvents.hurt(event => {//星爆
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:nova_guard') return
+    if (player.mainHandItem.id != 'pointblank:x_guard_1') return
     event.entity.potionEffects.add('minecraft:instant_damage', 1, 1)
 })
 EntityEvents.hurt(event => {//折射
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:refraction_dila') return
+    if (player.mainHandItem.id != 'pointblank:x_dila_1') return
     event.entity.potionEffects.add('minecraft:instant_damage', 1, 1)
 })
 
 EntityEvents.hurt(event => {//黑镜
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:rash_meteor_fist'
-        && player.mainHandItem.id != 'pointblank:ghost_pulse'
+    if (player.mainHandItem.id != 'pointblank:cr_meteor_fist_1'
+        && player.mainHandItem.id != 'pointblank:x_pulse_1'
     ) return
     if (getRandomInt() > 50) return
-    event.entity.potionEffects.add('fractionandspore:chaos', 20, 18)
+     event.entity.potionEffects.add('fractionandspore:chaos', 60, 18)
 })
 EntityEvents.hurt(event => {//辛迪加
     let player = event.getSource().getPlayer()
     if (!player) return
-    if (player.mainHandItem.id != 'pointblank:syndicate_demeanour') return
+    if (player.mainHandItem.id != 'pointblank:cr_demeanour_1') return
     event.entity.removeAllEffects()
 })
