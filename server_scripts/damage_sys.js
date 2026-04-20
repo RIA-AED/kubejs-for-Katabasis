74e0a5bb6e4f7ac0fe44a9beca9f56a597ac794f
liziluyu <2498281038@qq.com>
2026-04-20 14:27:38 +0800

    feat:伤害系统

---
+++ server_scripts/damage_sys.js
@@ -0,0 +1,66 @@
+
+let armorTypeDivide = [40, 100]
+
+let damageTypeDivide = [4.5, 9]
+
+let midDamageList = ["pointblank:st_rasword"]
+
+let midArmorList = ["spore:sieger"]
+
+let damageMatrix = [
+    [1, 0.7, 0],
+    [1, 1, 0.5],
+    [1, 1, 1]
+]
+
+EntityEvents.hurt(event => {
+    let armorLevel = 0
+    let damageLevel = 0
+    if (event.entity.maxHealth >= armorTypeDivide[0]) armorLevel = 1
+    if (event.entity.maxHealth >= armorTypeDivide[1]) armorLevel = 2
+    if (event.damage >= damageTypeDivide[0]) damageLevel = 1
+    if (event.damage >= damageTypeDivide[1]) damageLevel = 2
+
+    if (event.entity.tags.contains("lightArmor")) armorLevel = 0
+    if (event.entity.tags.contains("midArmor")) armorLevel = 1
+    if (event.entity.tags.contains("heavyArmor")) armorLevel = 2
+
+    if (event.entity.hasEffect("spore:corrosion")) armorLevel -= 1
+    if (armorLevel < 0) armorLevel = 0
+
+    let player = event.getSource().getPlayer()
+    if (player) {
+        midDamageList.forEach(it => {
+            if (it == player.mainHandItem.id) {
+                //event.server.tell(player.mainHandItem.id)
+                damageLevel = 1
+            }
+        })
+    }
+
+    let factor = damageMatrix[damageLevel][armorLevel]
+
+    //if (player) event.server.tell(factor)
+
+    if (factor < 1 && factor != 0) {
+        event.entity.heal(event.damage * (1 - factor))
+
+        if (player) {
+            playsound(event.server, player.block, "minecraft:block.bone_block.break", 2, 1)
+        }
+    }
+    if (factor == 0) {
+        event.entity.heal(event.damage)
+        let player = event.getSource().getPlayer()
+        if (player) {
+            playsound(event.server, player.block, "minecraft:block.anvil.land", 0.5, 2)
+        }
+    }
+})
+
+
+midArmorList.forEach(it => {
+    EntityEvents.spawned(it,event => {
+        event.entity.addTag("midArmor")
+    })
+})
--- server_scripts/util.js
+++ server_scripts/util.js
@@ -1,5 +1,5 @@
-ServerEvents.loaded(event=>{
-    event.server.scheduleInTicks(20,function(callback){
+ServerEvents.loaded(event => {
+    event.server.scheduleInTicks(20, function (callback) {
         event.server.tell("早上好中国现在我有CDU")
     })
 })
@@ -23,6 +23,19 @@ function randint(min, max) {
  */
 function playsound(server, block, soundId, maxVolum, pitch) {
     server.runCommandSilent(`execute positioned ${block.x} ${block.y} ${block.z} run playsound ${soundId} block @a[distance=..20] ${block.x} ${block.y} ${block.z} ${maxVolum} ${pitch} ${maxVolum / 2}`)
+}
+
+/**
+ * 发声（仅限单玩家）
+ * @param {Internal.MinecraftServer} server
+ * @param {Internal.BlockContainerJS} block
+ * @param {String} targetPlayerName
+ * @param {String} soundId
+ * @param {Number} maxVolum
+ * @param {Number} pitch
+ */
+function playsoundForPlayer(server, block, targetPlayerName, soundId, maxVolum, pitch) {
+    server.runCommandSilent(`execute positioned ${block.x} ${block.y} ${block.z} run playsound ${soundId} block ${targetPlayerName} ${block.x} ${block.y} ${block.z} ${maxVolum} ${pitch} ${maxVolum / 2}`)
 }
 
 /**
